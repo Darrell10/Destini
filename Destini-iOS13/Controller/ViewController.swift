f23fa54faee99ec6e2da59e8bb091d57b0e0b40b
Frederick PORT <developpeur@stud-ios.fr>
2020-10-31 15:39:13 +0100

    Ajout des commentaires

--- Destini-iOS13/Controller/ViewController.swift
+++ Destini-iOS13/Controller/ViewController.swift
@@ -8,17 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+    
+    // MARK: - Property
+    
+    var storyBrain = StoryBrain()
+    
+    // MARK: - Outlets
 
     @IBOutlet weak var storyLabel: UILabel!
     @IBOutlet weak var choice1Button: UIButton!
     @IBOutlet weak var choice2Button: UIButton!
     
+    // MARK: - Load view
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUI()
+    }
+    
+    // MARK: - Functions
+    
+    private func updateUI() {
+        storyLabel.text = storyBrain.getTitleStory()
+        choice1Button.setTitle(storyBrain.getTitleButton1(), for: .normal)
+        choice2Button.setTitle(storyBrain.getTitleButton2(), for: .normal)
+    }
+    
+    // MARK: - Actions
+    
+    @IBAction func choiceMade(_ sender: UIButton) {
+        guard let userAnswer = sender.currentTitle else {return}
+        storyBrain.nextStory(userChoice: userAnswer)
+        updateUI()
 
     }
-
-
 }
 
